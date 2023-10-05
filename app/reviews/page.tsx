ac88e4117961746ec9f697e8c01cd9de9f3a95fd
mikaelpac <mikaelpac@gmail.com>
2023-10-05 17:52:55 +0300

    added footer, folder structure cleanup

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,5 +1,42 @@
+"use client";
+import Container from "@/components/Container";
+import { Button } from "@/components/ui/button";
+
 const Reviews = () => {
-  return <div>Reviews</div>;
+  const apiKey = "";
+  const artistName = "The Contortionist";
+  const albumName = "Exoplanet"; // Replace with the correct album name
+  const apiUrl = `http://ws.audioscrobbler.com/2.0/?method=album.getInfo&api_key=${apiKey}&artist=${encodeURIComponent(
+    artistName
+  )}&album=${encodeURIComponent(albumName)}&format=json`;
+
+  const handleClick = () => {
+    fetch(apiUrl)
+      .then((response) => {
+        if (!response.ok) {
+          console.log(response);
+          throw new Error("Network response was not ok");
+        }
+        return response.json();
+      })
+      .then((data) => {
+        console.log(data);
+        // Handle the response data here.
+        const artistInfo = data.artist;
+        console.log(artistInfo);
+      })
+      .catch((error) => {
+        // Handle errors here.
+        console.log("Error:", error);
+      });
+  };
+
+  return (
+    <Container>
+      <div>Reviews</div>
+      <Button onClick={handleClick}>Submit review</Button>
+    </Container>
+  );
 };
 
 export default Reviews;
