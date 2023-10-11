7061db82ca9dab77c9fbbe9244d9c273fff62960
mikaelpac <mikaelpac@gmail.com>
2023-10-11 20:04:31 +0300

    rudimentary album image integration

--- app/(reviews)/submit-review/page.tsx
+++ app/(reviews)/submit-review/page.tsx
@@ -5,6 +5,7 @@ import ArtistSelect from "@/components/reviews/artist-select";
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import AlbumSelect from "@/components/reviews/album-select";
+import Image from "next/image";
 
 const SubmitReview = () => {
   const [selectedArtist, setSelectedArtist] = useState<string>("");
@@ -30,6 +31,11 @@ const SubmitReview = () => {
   return (
     <Container>
       <div className="text-2xl text-center mt-12">Submit a Review</div>
+      {selectedArtist && selectedAlbum && (
+        <div className="text-xl text-center mt-12">
+          {selectedArtist} - {selectedAlbum.name}
+        </div>
+      )}
 
       <div className="w-full  max-w-4xl mx-auto mt-12 mb-12 rounded-md">
         <div className="max-w-2xl flex flex-col justify-center mx-auto">
@@ -39,6 +45,19 @@ const SubmitReview = () => {
               selectedArtist={selectedArtist}
               onAlbumSelect={handleSelectedAlbum}
             />
+          )}
+
+          {selectedAlbum && selectedAlbum.images[2] && (
+            <div className="mt-4">
+              <Image
+                loader={}
+                src={selectedAlbum.images[2]}
+                alt="Album cover"
+                width={300}
+                className="rounded-sm"
+                height={300}
+              />
+            </div>
           )}
         </div>
       </div>
